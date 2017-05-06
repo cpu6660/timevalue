bf6d2db8a58a96ca081444d9198ca83651087026
chenp <chenp@2345.com>
2017-05-06 20:22:18 +0800

    update

---
+++ application/wechat/controller/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 微信公众号入口
+ * Created by PhpStorm.
+ * User: pengchen
+ * Date: 17/5/6
+ * Time: 20:11
+ */
+namespace app\wechat\controller;
+
+class Index {
+	public function index() {
+		echo "wechat ceshi";
+	}
+}
+
