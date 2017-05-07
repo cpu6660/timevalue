6c92fe97ae23f63683fab483a196158f8816ffc7
chenp <chenp@2345.com>
2017-05-07 11:33:08 +0800

    update

--- application/common.php
+++ application/common.php
@@ -10,3 +10,19 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
+
+//如果检查请求是否是post请求的方法不存在,则定义
+if (!function_exists("is_post_method"))
+{
+	function is_post_method()
+	{
+		if (isset($_SERVER["REQUEST_METHOD"]) && !strcasecmp($_SERVER['REQUEST_METHOD'], 'POST'))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
