16cb26c05455fa1fda0aa5e38fc5e6b09e0d2c52
chenp <chenp@2345.com>
2017-05-07 11:13:38 +0800

    update

--- application/api/behavior/EncryptBehavior.php
+++ application/api/behavior/EncryptBehavior.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * 目前统一接口get post 传回接口的数据的key设置为_E_JSON,
+ * 通过_E_JSON传递加密后的数据
  * Created by PhpStorm.
  * User: pengchen
  * Date: 17/5/6
@@ -10,17 +12,21 @@ namespace app\api\behavior;
 class EncryptBehavior {
 
 	/**
-	 * 功能:加密
+	 * 功能:解密
 	 */
 	public function appInit(&$params) {
-		echo "加密成功\n";
+		if(is_post_mehtod()){
+			echo "post请求";
+		}else{
+			echo "非post请求";
+		}
 	}
 
 	/**
-	 * 功能:解密
+	 * 功能:加密
 	 *
 	 */
 	public function appEnd(&$params) {
-		echo "解密成功\n";
+		echo "加密成功\n";
 	}
 }
